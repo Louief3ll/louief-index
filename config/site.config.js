f127e91b41bcfb5e6bcfabd8fa823b100c84a30a
Tresillocrack <tresillo2017@gmail.com>
2023-05-18 19:43:14 +0200

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Test', '/ROMs/🔒 Private', '/🏢 Work JC1'],
+  protectedRoutes: ['/Test', '/ROMs/🔒 Private'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:contact@tomasps.tk',
